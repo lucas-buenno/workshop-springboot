be1db3245ab2ca3a46e532a036f10b54f5216d4d
Lucas Bueno <lucasbuenno@outlook.com>
2024-07-30 08:07:02 -0300

    Component registration

--- src/main/java/com/projectcourse/resource/UserResource.java
+++ src/main/java/com/projectcourse/resource/UserResource.java
@@ -1,19 +1,29 @@
 package com.projectcourse.resource;
 
 import com.projectcourse.entities.User;
+import com.projectcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@email.com","99999999","123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
